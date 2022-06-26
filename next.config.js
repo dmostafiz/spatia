97f2bf9f1daa1b9339a73d1b6073523fba89bf24
mostafizrr2 <dev.mostafiz@gmail.com>
2022-06-27 00:40:30 +0600

    esmExperimental False

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  staticPageGenerationTimeout: 240,
+  experimental: {
+    esmExternals: false,
+  },
 }
 
 module.exports = nextConfig
