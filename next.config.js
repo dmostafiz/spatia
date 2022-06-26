78280162978c9d3f76599bed98756f365da668e2
mostafizrr2 <dev.mostafiz@gmail.com>
2022-06-27 01:25:23 +0600

    hmm

--- next.config.js
+++ next.config.js
@@ -1,10 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  staticPageGenerationTimeout: 240,
-  experimental: {
-    esmExternals: false,
-  },
+
 }
 
 module.exports = nextConfig
