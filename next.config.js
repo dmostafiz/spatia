deaec7e073dff1dbd6f927488111e55197db18e0
mostafizrr2 <dev.mostafiz@gmail.com>
2022-07-15 03:24:33 +0600

    hello world

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true
+  reactStrictMode: true,
+  env: {
+    ENVIRONMENT: process.env.ENVIRONMENT,
+  }
 }
 
 module.exports = nextConfig
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -4,14 +4,24 @@ import { ChakraProvider } from '@chakra-ui/react'
 import appTheme from '../../Helpers/_theme'
 import axios from 'axios'
 import useToken from './../Hooks/useToken';
+import { useEffect } from 'react';
 
-axios.defaults.baseURL = "http://localhost:3000/api"
 
 function MyApp({ Component, pageProps }) {
 
+
+  useEffect(() => {
+
+    console.log('window.location.hostname ########', window.location.hostname)
+    console.log('Environment ########', process.env.ENVIRONMENT)
+
+  }, [])
+
+  axios.defaults.baseURL = process.env.ENVIRONMENT == 'development' ? 'http://localhost:3000/api' : 'https://dashboard.heroku.com/api'
+
   axios.defaults.headers.common['Authorization'] = useToken();
   axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
-  
+
   return <ChakraProvider theme={appTheme}>
     <Component {...pageProps} />
   </ChakraProvider>
