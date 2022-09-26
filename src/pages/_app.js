df073fe0c77f47441303aed87f40a34b78d28692
dmostafiz <dev.mostafiz@gmail.com>
2022-09-26 21:27:58 +0600

    recent loading issue solved

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -33,8 +33,8 @@ function MyApp({ Component, pageProps }) {
   // }, [])
 
   // axios.defaults.baseURL = process.env.ENVIRONMENT == 'development' ? 'http://localhost:3000/api' : 'https://spacom.herokuapp.com/api'
-  axios.defaults.baseURL = 'http://localhost:3000/api'
-  // axios.defaults.baseURL = 'https://spacom.herokuapp.com/api'
+  // axios.defaults.baseURL = 'http://localhost:3000/api'
+  axios.defaults.baseURL = 'https://spacom.herokuapp.com/api'
   axios.defaults.headers.common['Authorization'] = useToken();
   axios.defaults.headers.post['Content-Type'] = 'application/json';
 
